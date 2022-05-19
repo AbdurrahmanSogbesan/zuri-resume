5e847e71e4e417e45d65b1774b9ddffd3087ad1b
Abdurrahman Sogbesan <abdurrahman0803@gmail.com>
2022-05-20 00:57:09 +0100

    adjusted placeholder image

--- src/components/Hobbies/index.js
+++ src/components/Hobbies/index.js
@@ -15,15 +15,7 @@ function Hobbies({ name, description, image }) {
       />
       <Modal title={name} show={show} onClose={() => setShow(false)}>
         <Text text={description} />
-        <img
-          src={
-            image ||
-            "https://cdn.shopify.com/s/files/1/0506/0316/3824/t/6/assets/placeholder_2048x.png?v=113555733946226816651623079373"
-          }
-          alt=""
-          className="hobbyimg"
-          width="60%"
-        />
+        <img src={image} alt="" className="hobbyimg" width="60%" />
       </Modal>
     </div>
   );
